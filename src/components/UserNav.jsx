3f1146bf742f42f142e05a66819be84dfbb89392
Mauro Brandan <maurobrandan.mb@gmail.com>
2023-08-31 12:57:06 -0300

    Don't show two login buttons in /login

--- src/components/UserNav.jsx
+++ src/components/UserNav.jsx
@@ -1,16 +1,21 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { useAuth0 } from '@auth0/auth0-react'
 import { LoginButton } from './Auth'
 import { AvatarIcon } from './Icons'
-import { Link } from 'wouter'
+import { Link, useLocation } from 'wouter'
 
 export function UserNav () {
   const [isShow, setIsShow] = useState(false)
   const { user, isAuthenticated, isLoading } = useAuth0()
+  const [location] = useLocation()
 
   const handleClick = () => {
     setIsShow(!isShow)
   }
+
+  useEffect(() => {
+    setIsShow(false)
+  }, [location])
 
   return (
     <div>
@@ -22,7 +27,7 @@ export function UserNav () {
           <Link href='/user' className='w-52 text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2'>
             Mi cuenta
           </Link>
-          <LoginButton />
+          {!(location === '/login') && <LoginButton />}
         </div>
       </div>
     </div>
